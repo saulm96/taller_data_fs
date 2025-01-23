c7092eb8db802787e372c60106f2783831f372d7
saulm96 <saulmorahernandez96@gmail.com>
2025-01-23 10:22:11 +0100

    Component folder

---
+++ client/mi-aplicacion/src/components/Form.jsx
@@ -0,0 +1,10 @@
+const Form = () => {
+    return (
+        <form>
+        <input type="text" />
+        <button type="submit">Enviar</button>
+        </form>
+    );
+    }
+
+export default Form;
